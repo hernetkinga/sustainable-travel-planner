22f730c210550d712d35bd14d92e443c3a3960fc
Kinga <01168834@pw.edu.pl>
2025-06-24 14:31:01 +0200

    updated config.example

--- config/config.example.php
+++ config/config.example.php
@@ -4,10 +4,10 @@
  * Copy this file to config.php and insert your actual credentials.
  */
 
-// 🔑 Google Maps API Key
+// Google Maps API Key
 // Get it from Google Cloud Console: https://console.cloud.google.com/
 define('GOOGLE_MAPS_API_KEY', 'YOUR_GOOGLE_MAPS_API_KEY');
 
-// 🔑 WeatherAPI Key
+// WeatherAPI Key
 // Get it from https://www.weatherapi.com/
 define('WEATHER_API_KEY', 'YOUR_WEATHER_API_KEY');
